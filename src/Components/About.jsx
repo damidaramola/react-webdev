476a9317c4173355af341f91a667396861dec18c
Dami Daramola <110638513+damidaramola@users.noreply.github.com>
2024-02-03 22:52:25 +0000

    add background image

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/jigar-panchal-bikKtf6mgZ8-unsplash.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -43,7 +43,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my software development experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
